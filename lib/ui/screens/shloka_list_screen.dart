2b80f3eaf6c76df3e482e82f096650009254eac7
Digish Pandya <digish.pandya@gmail.com>
2025-09-08 13:02:59 +0530

    Continuous playback option

--- lib/ui/screens/shloka_list_screen.dart
+++ lib/ui/screens/shloka_list_screen.dart
@@ -2,10 +2,11 @@ import 'package:bhagvadgeeta/ui/widgets/simple_gradient_background.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
+import '../../models/shloka_result.dart';
+import '../../providers/audio_provider.dart';
 import '../../providers/shloka_list_provider.dart';
 import '../widgets/full_shloka_card.dart';
 import '../../data/static_data.dart';
-import '../../data/database_helper.dart';
 import '../../data/database_helper_interface.dart';
 
 class ShlokaListScreen extends StatefulWidget {
@@ -18,11 +19,19 @@ class ShlokaListScreen extends StatefulWidget {
 
 class _ShlokaListScreenState extends State<ShlokaListScreen> {
   final ItemScrollController itemScrollController = ItemScrollController();
+  int? _lastScrolledIndex;
+  bool _isContinuousPlayEnabled = false;
+
+  // To detect when a shloka finishes playing
+  String? _previousPlayingId;
+  PlaybackState? _previousPlaybackState;
 
   void _scrollToIndex(int index) {
+    if (_lastScrolledIndex == index) return; // Avoid redundant scrolls
     // A post-frame callback ensures that the list has been built and is ready to be scrolled.
     WidgetsBinding.instance.addPostFrameCallback((_) {
       if (itemScrollController.isAttached) {
+        _lastScrolledIndex = index;
         itemScrollController.scrollTo(
           index: index,
           duration: const Duration(milliseconds: 700),
@@ -35,7 +44,6 @@ class _ShlokaListScreenState extends State<ShlokaListScreen> {
 
   @override
   Widget build(BuildContext context) {
-
     final dbHelper = Provider.of<DatabaseHelperInterface>(context, listen: false);
 
     // Try to parse chapter number for showing the emblem header.
@@ -53,16 +61,39 @@ class _ShlokaListScreenState extends State<ShlokaListScreen> {
                   fontWeight: FontWeight.bold,
                 ),
           ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // The switch is placed first.
+                  Switch(
+                    value: _isContinuousPlayEnabled,
+                    onChanged: (value) {
+                      setState(() {
+                        _isContinuousPlayEnabled = value;
+                      });
+                    },
+                    // This makes the switch more compact vertically.
+                    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  ),
+                  // The label is placed below the switch.
+                  Text('Non-Stop Play', style: Theme.of(context).textTheme.bodySmall?.copyWith(color: Colors.white70))
+                ],
+              ),
+            ),
+          ],
           backgroundColor: Colors.black.withOpacity(0.4),
-          elevation: 0, 
+          elevation: 0,
           centerTitle: true,
         ),
         extendBodyBehindAppBar: true,
         body: Stack(
           children: [
             const SimpleGradientBackground(),
-            Consumer<ShlokaListProvider>(
-              builder: (context, provider, child) {
+            Consumer2<ShlokaListProvider, AudioProvider>(
+              builder: (context, provider, audioProvider, child) {
                 if (provider.isLoading) {
                   return const Center(child: CircularProgressIndicator());
                 }
@@ -85,6 +116,43 @@ class _ShlokaListScreenState extends State<ShlokaListScreen> {
                   provider.clearScrollIndex();
                 }
 
+                final shlokas = provider.shlokas;
+                final currentPlayingId = audioProvider.currentPlayingShlokaId;
+                final currentPlaybackState = audioProvider.playbackState;
+
+                // --- AUTO-SCROLL LOGIC ---
+                if (currentPlayingId != null) {
+                  final playingIndex = shlokas.indexWhere(
+                      (s) => '${s.chapterNo}.${s.shlokNo}' == currentPlayingId);
+                  if (playingIndex != -1) {
+                    _scrollToIndex(playingIndex);
+                  }
+                } else {
+                  _lastScrolledIndex = null;
+                }
+
+                // --- CONTINUOUS PLAY LOGIC ---
+                WidgetsBinding.instance.addPostFrameCallback((_) {
+                  // Detect completion: previous state was playing, current is not.
+                  if (_previousPlayingId != null &&
+                      _previousPlaybackState == PlaybackState.playing &&
+                      currentPlayingId != _previousPlayingId) {
+                    if (_isContinuousPlayEnabled) {
+                      final lastPlayedIndex = shlokas.indexWhere((s) =>
+                          '${s.chapterNo}.${s.shlokNo}' == _previousPlayingId);
+
+                      if (lastPlayedIndex != -1 &&
+                          lastPlayedIndex < shlokas.length - 1) {
+                        final ShlokaResult nextShloka =
+                            shlokas[lastPlayedIndex + 1];
+                        audioProvider.playOrPauseShloka(nextShloka);
+                      }
+                    }
+                  }
+                  _previousPlayingId = currentPlayingId;
+                  _previousPlaybackState = currentPlaybackState;
+                });
+
                 return ScrollablePositionedList.builder(
                   itemScrollController: itemScrollController,
                   itemCount: provider.shlokas.length,
@@ -94,8 +162,8 @@ class _ShlokaListScreenState extends State<ShlokaListScreen> {
                     bottom: 20,
                   ),
                   itemBuilder: (context, index) {
-                    final shloka = provider.shlokas[index];
-                    
+                    final shloka = shlokas[index];
+
                     // If this is a chapter view, display a static emblem header above the first shloka.
                     if (index == 0 && chapterNumber != null) {
                       return Column(
