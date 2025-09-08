7b4a1c4635693f636f58ca43391208fcc01e8ce2
Digish Pandya <digish.pandya@gmail.com>
2025-09-08 15:49:10 +0530

    Copyright update

--- lib/data/database_helper_interface.dart
+++ lib/data/database_helper_interface.dart
@@ -1,3 +1,16 @@
+/* 
+*  © 2025 Digish Pandya. All rights reserved.
+*
+*  This mobile application, "Shrimad Bhagavad Gita," including its code, design, and original content, is released under the [MIT License] unless otherwise noted.
+*
+*  The sacred text of the Bhagavad Gita, as presented herein, is in the public domain. Translations, interpretations, UI elements, and artistic representations created by the developer are protected under copyright law.
+*
+*  This app is offered in the spirit of dharma and shared learning. You are welcome to use, modify, and distribute the source code under the terms of the MIT License. However, please preserve the integrity of the spiritual message and credit the original contributors where due.
+*
+*  For licensing details, see the LICENSE file in the repository.
+*
+**/
+
 import '../models/shloka_result.dart';
 import '../models/word_result.dart';
 
--- lib/providers/shloka_list_provider.dart
+++ lib/providers/shloka_list_provider.dart
@@ -1,3 +1,16 @@
+/* 
+*  © 2025 Digish Pandya. All rights reserved.
+*
+*  This mobile application, "Shrimad Bhagavad Gita," including its code, design, and original content, is released under the [MIT License] unless otherwise noted.
+*
+*  The sacred text of the Bhagavad Gita, as presented herein, is in the public domain. Translations, interpretations, UI elements, and artistic representations created by the developer are protected under copyright law.
+*
+*  This app is offered in the spirit of dharma and shared learning. You are welcome to use, modify, and distribute the source code under the terms of the MIT License. However, please preserve the integrity of the spiritual message and credit the original contributors where due.
+*
+*  For licensing details, see the LICENSE file in the repository.
+*
+**/
+
 import 'package:flutter/material.dart';
 import '../data/database_helper.dart';
 import '../models/shloka_result.dart';
