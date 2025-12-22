c2899960ce846612eb7a50634980d1b204c0f5ce
Digish Pandya <digish.pandya@gmail.com>
2025-12-22 15:29:46 +0530

    chore: iOS app release 25.0.2+1

--- lib/data/database_helper.dart
+++ lib/data/database_helper.dart
@@ -16,6 +16,4 @@
 // The rest of your app will ONLY import this file.
 
 export 'database_helper_unsupported.dart'
-    if (dart.library.html) 'database_helper_web.dart'
     if (dart.library.io) 'database_helper_mobile.dart';
-
