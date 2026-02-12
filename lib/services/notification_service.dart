140905a08a8d8ff8b26257da250a043136518842
Digish Pandya <digish.pandya@gmail.com>
2026-02-12 08:44:27 +0530

    fix: Use flutter_timezone to accurately determine and set the local timezone for notification scheduling.

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -2,6 +2,7 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest.dart' as tz;
+import 'package:flutter_timezone/flutter_timezone.dart';
 import 'daily_message_service.dart';
 
 class NotificationService {
@@ -14,6 +15,8 @@ class NotificationService {
 
   Future<void> init() async {
     tz.initializeTimeZones();
+    final String timeZoneName = await FlutterTimezone.getLocalTimezone();
+    tz.setLocalLocation(tz.getLocation(timeZoneName));
 
     const AndroidInitializationSettings initializationSettingsAndroid =
         AndroidInitializationSettings('@mipmap/ic_launcher');
